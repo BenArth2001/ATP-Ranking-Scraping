96d0e0787d96dfc432f5350d2f0aae76d3702702
Alan <alanohayon@gmail.com>
2024-03-06 11:16:44 +0100

    ajout doss_indicateur

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ from database.db import engine
 from models.models import Base
 from routes import scrape_routes
 from routes import db_routes
-
+from routes import indicateur_routes
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 app.include_router(scrape_routes.router, prefix="/scrape", tags=["scrape"])
 app.include_router(db_routes.router, prefix="/database", tags=["database"])
+app.include_router(db_routes.router, prefix="/indicateur", tags=["indicateur"])
